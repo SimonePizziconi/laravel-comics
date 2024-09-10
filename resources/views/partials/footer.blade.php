7ea264b1075dad68c3a616c3176c1c3e183ac7ad
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-10 16:37:22 +0200

    feat: create footer.php and footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,149 @@
-<footer class="my-5 text-center">
-    <p>by UdU</p>
+<footer>
+    <section class="info-section">
+        <ul>
+            <li>
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}"
+                        alt="DIGITAL COMICS">DIGITAL
+                    COMICS</a>
+            </li>
+            <li>
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}"
+                        alt="DIGITAL COMICS">DC
+                    MERCHANDISE</a>
+            </li>
+            <li>
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}"
+                        alt="DIGITAL COMICS">SUBSCRIPTION</a>
+            </li>
+            <li>
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}"
+                        alt="">COMIC SHOP
+                    LOCATOR</a>
+            </li>
+            <li>
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}"
+                        alt="DIGITAL COMICS">DC POWER VISA</a>
+            </li>
+
+        </ul>
+    </section>
+    <section class="list-section">
+        <div>
+            <ul>
+                <li>
+                    <h3>DC COMICS</h3>
+                </li>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+                <li>
+                    <h3>SHOP</h3>
+                </li>
+                <li>
+                    <a href="#">Shop DC</a>
+                </li>
+                <li>
+                    <a href="#">Shop DC Collectibles</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <h3>DC</h3>
+                </li>
+                <li>
+                    <a href="#">Terme of the use</a>
+                </li>
+                <li>
+                    <a href="#">Privacy policy(New)</a>
+                </li>
+                <li>
+                    <a href="#">Ad Choices</a>
+                </li>
+                <li>
+                    <a href="#">Advertising</a>
+                </li>
+                <li>
+                    <a href="#">Jobs</a>
+                </li>
+                <li>
+                    <a href="#">Subscriptions</a>
+                </li>
+                <li>
+                    <a href="#">Talent Worksshops</a>
+                </li>
+                <li>
+                    <a href="#">CPSC Certificates</a>
+                </li>
+                <li>
+                    <a href="#">Ratings</a>
+                </li>
+                <li>
+                    <a href="#">Shop Help</a>
+                </li>
+                <li>
+                    <a href="#">Contact Us</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <h3>SITES</h3>
+                </li>
+                <li>
+                    <a href="#">DC</a>
+                </li>
+                <li>
+                    <a href="#">MAD Magazine</a>
+                </li>
+                <li>
+                    <a href="#">DC Kids</a>
+                </li>
+                <li>
+                    <a href="#">DC Universe</a>
+                </li>
+                <li>
+                    <a href="#">DC Power Visa</a>
+                </li>
+            </ul>
+            <div class="img-container">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="sign-up-section">
+        <div class="container-icon">
+            <button>
+                SIGN-UP NOW
+            </button>
+
+            <div class="icon">
+                <span>FOLLOW US</span>
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="Youtube">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope">
+            </div>
+        </div>
+
+    </section>
 </footer>
